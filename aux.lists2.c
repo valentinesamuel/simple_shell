4073853e1d286c879fd597da0e062e61d1a95bb8
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-04 07:29:18 -0700

    completed aux_list and aux_lists2

--- aux.lists2.c
+++ aux.lists2.c
@@ -34,3 +34,27 @@ r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
 			temp = temp->next;
 		temp->next = new;
 	}
+	return (*head);
+}
+
+/**
+ * free_rvar_list - frees a r_var list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
