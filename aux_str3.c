86df2a0e822010d389dcdbd3a8d32436a1acf152
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-06 01:56:25 -0700

    aux str3 file

---
+++ aux_str3.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
+void rev_string(char *s)
+{
+	int count = 0, i, j;
+	char *str, temp;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
