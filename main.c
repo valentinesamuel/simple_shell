12a9098f07aa844443337bacfa217a24d51d9d33
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-06 05:28:06 -0700

    main.c file

--- main.c
+++ main.c
@@ -49,3 +49,25 @@ void set_data(data_shell *datash, char **av)
 	datash->_environ[i] = NULL;
 	datash->pid = aux_itoa(getpid());
 }
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
