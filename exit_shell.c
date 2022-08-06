8c9e5f7646aac09c54ba38967a9828481d71c9bf
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-06 05:51:33 -0700

    corrected exit shell

--- exit_shell.c
+++ exit_shell.c
@@ -6,17 +6,26 @@
  * @datash: data relevant (status and args)
  * Return: 0 on success.
  */
-int exec_line(data_shell *datash)
+int exit_shell(data_shell *datash)
 {
-	int (*builtin)(data_shell *datash);
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
 
-	if (datash->args[0] == NULL)
-		return (1);
-
-	builtin = get_builtin(datash->args[0]);
-
-	if (builtin != NULL)
-		return (builtin(datash));
-
-	return (cmd_exec(datash));
+	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
+	}
+	return (0);
 }
