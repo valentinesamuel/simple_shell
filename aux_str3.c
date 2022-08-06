5a2aa4a5aeebdec0d97a527bab46019218bd91cc
valentinesamuel <valentinesamuel2580@gmail.com>
2022-08-06 03:27:55 -0700

    completed second batch

--- aux_str3.c
+++ aux_str3.c
@@ -16,3 +16,15 @@ void rev_string(char *s)
 			break;
 		count++;
 	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}
--- split.c
+++ split.c
@@ -96,3 +96,117 @@ void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
 	loop_sep = 1;
 	ls_s = *list_s;
 	ls_l = *list_l;
+
+	while (ls_s != NULL && loop_sep)
+	{
+		if (datash->status == 0)
+		{
+			if (ls_s->separator == '&' || ls_s->separator == ';')
+				loop_sep = 0;
+			if (ls_s->separator == '|')
+				ls_l = ls_l->next, ls_s = ls_s->next;
+		}
+		else
+		{
+			if (ls_s->separator == '|' || ls_s->separator == ';')
+				loop_sep = 0;
+			if (ls_s->separator == '&')
+				ls_l = ls_l->next, ls_s = ls_s->next;
+		}
+		if (ls_s != NULL && !loop_sep)
+			ls_s = ls_s->next;
+	}
+
+	*list_s = ls_s;
+	*list_l = ls_l;
+}
+
+/**
+ * split_commands - splits command lines according to
+ * the separators ;, | and &, and executes them
+ *
+ * @datash: data structure
+ * @input: input string
+ * Return: 0 to exit, 1 to continue
+ */
+int split_commands(data_shell *datash, char *input)
+{
+
+	sep_list *head_s, *list_s;
+	line_list *head_l, *list_l;
+	int loop;
+
+	head_s = NULL;
+	head_l = NULL;
+
+	add_nodes(&head_s, &head_l, input);
+
+	list_s = head_s;
+	list_l = head_l;
+
+	while (list_l != NULL)
+	{
+		datash->input = list_l->line;
+		datash->args = split_line(datash->input);
+		loop = exec_line(datash);
+		free(datash->args);
+
+		if (loop == 0)
+			break;
+
+		go_next(&list_s, &list_l, datash);
+
+		if (list_l != NULL)
+			list_l = list_l->next;
+	}
+
+	free_sep_list(&head_s);
+	free_line_list(&head_l);
+
+	if (loop == 0)
+		return (0);
+	return (1);
+}
+
+/**
+ * split_line - tokenizes the input string
+ *
+ * @input: input string.
+ * Return: string splitted.
+ */
+char **split_line(char *input)
+{
+	size_t bsize;
+	size_t i;
+	char **tokens;
+	char *token;
+
+	bsize = TOK_BUFSIZE;
+	tokens = malloc(sizeof(char *) * (bsize));
+	if (tokens == NULL)
+	{
+		write(STDERR_FILENO, ": allocation error\n", 18);
+		exit(EXIT_FAILURE);
+	}
+
+	token = _strtok(input, TOK_DELIM);
+	tokens[0] = token;
+
+	for (i = 1; token != NULL; i++)
+	{
+		if (i == bsize)
+		{
+			bsize += TOK_BUFSIZE;
+			tokens = _reallocdp(tokens, i, sizeof(char *) * bsize);
+			if (tokens == NULL)
+			{
+				write(STDERR_FILENO, ": allocation error\n", 18);
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = _strtok(NULL, TOK_DELIM);
+		tokens[i] = token;
+	}
+
+	return (tokens);
+}
