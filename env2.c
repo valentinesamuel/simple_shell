1df45ec8d34f8dd64fcd786c1619c4f534e28ac1
nimikadejo <kadejotoluwanimi@gmail.com>
2022-08-06 05:20:01 -0700

    env 2

--- env2.c
+++ env2.c
@@ -56,3 +56,73 @@ void set_env(char *name, char *value, data_shell *datash)
 	datash->_environ[i] = copy_info(name, value);
 	datash->_environ[i + 1] = NULL;
 }
+/**
+ * _setenv - compares env variables names
+ * with the name passed.
+ * @datash: data relevant (env name and env value)
+ *
+ * Return: 1 on success.
+ */
+int _setenv(data_shell *datash)
+{
+
+	if (datash->args[1] == NULL || datash->args[2] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+
+	set_env(datash->args[1], datash->args[2], datash);
+
+	return (1);
+}
+
+/**
+ * _unsetenv - deletes a environment variable
+ *
+ * @datash: data relevant (env name)
+ *
+ * Return: 1 on success.
+ */
+int _unsetenv(data_shell *datash)
+{
+	char **realloc_environ;
+	char *var_env, *name_env;
+	int i, j, k;
+
+	if (datash->args[1] == NULL)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+	k = -1;
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		var_env = _strdup(datash->_environ[i]);
+		name_env = _strtok(var_env, "=");
+		if (_strcmp(name_env, datash->args[1]) == 0)
+		{
+			k = i;
+		}
+		free(var_env);
+	}
+	if (k == -1)
+	{
+		get_error(datash, -1);
+		return (1);
+	}
+	realloc_environ = malloc(sizeof(char *) * (i));
+	for (i = j = 0; datash->_environ[i]; i++)
+	{
+		if (i != k)
+		{
+			realloc_environ[j] = datash->_environ[i];
+			j++;
+		}
+	}
+	realloc_environ[j] = NULL;
+	free(datash->_environ[k]);
+	free(datash->_environ);
+	datash->_environ = realloc_environ;
+	return (1);
+}
